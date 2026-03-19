eb49972c645b82f04b5eacfe380998fb86694364
VITASETYUKOVA <_viktorija-@ukr.net>
2026-03-19 13:17:07 +0200

    fix

--- src/po/cart.page.js
+++ src/po/cart.page.js
@@ -5,10 +5,22 @@ class CartPage {
     }
 
     async validateItemInCart(itemName) {
-         const item = await $(`//div[@class="inventory_item_name" and text()="${itemName}"]`);
-        await expect(item).toBeDisplayed();
+   
+    const items = await $$('.inventory_item_name');
+
+    let found = false;
+    for (const el of items) {
+        const text = await el.getText();
+        if (text === itemName) {
+            await expect(el).toBeDisplayed();
+            found = true;
+            break;
+        }
     }
 
+    if (!found) {
+        throw new Error(`Item "${itemName}" not found in cart`);
+    }}
     async proceedToCheckout() {
         await this.btnCheckout.click();
     }
